b3ed9e4b308af7030ab2194b10a71cc363dc9f14
Kouki Nakayama <k.number.02@gmail.com>
2023-05-23 14:15:00 +0900

    openai モジュールインストール

--- Dockerfile
+++ Dockerfile
@@ -7,12 +7,15 @@ WORKDIR /src
 # pipを使ってpoetryをインストール
 RUN pip install poetry
 
-# poetryの定義ファイルをコピー (存在する場合)
-COPY pyproject.toml* poetry.lock* ./
+# poetryの定義ファイルをコピー
+COPY pyproject.toml poetry.lock ./
 
-# poetryでライブラリをインストール (pyproject.tomlが既にある場合)
-RUN poetry config virtualenvs.in-project true
-RUN if [ -f pyproject.toml ]; then poetry install --no-root; fi
+# poetryでライブラリをインストール
+RUN poetry config virtualenvs.create false \
+    && poetry install --no-root
+
+# openai moduleを明示的にインストール
+RUN poetry add openai
 
 # uvicornのサーバーを立ち上げる
-ENTRYPOINT ["poetry", "run", "uvicorn", "api.main:app", "--host", "0.0.0.0", "--reload"]
+CMD ["poetry", "run", "uvicorn", "api.main:app", "--host", "0.0.0.0", "--reload"]
--- api/main.py
+++ api/main.py
@@ -1,8 +1,62 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import openai
+from pydantic import BaseModel
 
 app = FastAPI()
 
+openai.api_key ='' # APIキーを設定する
 
-@app.get("/hello")
-async def hello():
-    return {"message": "hello world!"}
+class User(BaseModel):
+    id: str
+    name: str
+    messages: list = []
+
+class Message(BaseModel):
+    user_id: str
+    content: str
+
+# Store for users
+users = {}
+
+@app.post("/users/")
+def create_user(user: User):
+    if user.id not in users:
+        users[user.id] = user
+    return user
+
+@app.get("/users/{user_id}")
+def read_user(user_id: str):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="User not found")
+    return users[user_id]
+
+@app.post("/messages/")
+def create_message(message: Message):
+    if message.user_id not in users:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user = users[message.user_id]
+    user.messages.append(message.content)
+    chat_model = "gpt-3.5-turbo"
+
+    if openai.api_key == '':
+        response_content = "OpenAI API key is not set."
+    else:
+        response = openai.ChatCompletion.create(
+            model=chat_model,
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": message.content},
+            ]
+        )
+        response_content = response['choices'][0]['message']['content']
+
+    user.messages.append(response_content)
+    return response_content
+
+@app.get("/messages/{user_id}")
+def read_messages(user_id: str):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="User not found")
+    user = users[user_id]
+    return user.messages
