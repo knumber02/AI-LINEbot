b88e80f0e6dd3882d9630b01e446c043bdac5c77
Kouki Nakayama <k.number.02@gmail.com>
2023-05-23 14:56:25 +0900

    ロジック追加

--- api/main.py
+++ api/main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI, HTTPException
 import openai
 from pydantic import BaseModel
+import json
+
+# JSONファイルを開き、値を読み込む
+with open('config.json') as f:
+    data = json.load(f)
+
+# JSONからAPIキーを取得
+openai.api_key = data['OPENAI_API_KEY']
 
 app = FastAPI()
 
-openai.api_key ='' # APIキーを設定する
 
 class User(BaseModel):
     id: str
     name: str
+    personality: str = "You are an assistant that speaks like a cute girlfriend."
     messages: list = []
 
 class Message(BaseModel):
@@ -16,7 +24,7 @@ class Message(BaseModel):
     content: str
 
 # Store for users
-users = {}
+users = {'default': User(id='default', name='Default User', messages=[])}
 
 @app.post("/users/")
 def create_user(user: User):
@@ -36,22 +44,34 @@ def create_message(message: Message):
         raise HTTPException(status_code=404, detail="User not found")
 
     user = users[message.user_id]
-    user.messages.append(message.content)
+    user.messages.append({"role": "user", "content": message.content})
     chat_model = "gpt-3.5-turbo"
 
     if openai.api_key == '':
         response_content = "OpenAI API key is not set."
     else:
-        response = openai.ChatCompletion.create(
-            model=chat_model,
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": message.content},
-            ]
-        )
-        response_content = response['choices'][0]['message']['content']
+        # Limit the conversation history to the latest 10 messages to avoid reaching the token limit.
+        conversation_history = user.messages[-10:]
 
-    user.messages.append(response_content)
+        conversation_history.insert(0, {"role": "system", "content": user.personality})
+
+        try:
+            response = openai.ChatCompletion.create(
+                model=chat_model,
+                messages=conversation_history
+            )
+            response_content = response['choices'][0]['message']['content']
+
+            # Filter out inappropriate content (This is a simplistic approach.
+            # More sophisticated content moderation may be needed depending on your use case.)
+            inappropriate_words = ['inappropriate', 'offensive']
+            if any(word in response_content for word in inappropriate_words):
+                response_content = "I'm sorry, but I can't assist with that."
+
+        except Exception as e:
+            response_content = f"Error: {str(e)}"
+
+    user.messages.append({"role": "assistant", "content": response_content})
     return response_content
 
 @app.get("/messages/{user_id}")
@@ -60,3 +80,10 @@ def read_messages(user_id: str):
         raise HTTPException(status_code=404, detail="User not found")
     user = users[user_id]
     return user.messages
+
+@app.put("/users/{user_id}/personality")
+def set_personality(user_id: str, personality: str):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="User not found")
+    users[user_id].personality = personality
+    return {"message": "Personality set successfully."}
