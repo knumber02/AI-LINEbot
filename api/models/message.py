eff27e9e0fa500169944694d9820fe83c0722cb7
knumber02 <k.number.02@gmail.com>
2025-04-12 07:56:12 +0900

    fix: Messageモデル修正

--- api/models/message.py
+++ api/models/message.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class Message(BaseModel):
+    id: str
     user_id: str
-    content: str 
+    content: str
+    created_at: datetime = datetime.now()
