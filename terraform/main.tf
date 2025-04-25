3cfd7f0e53ef61ff3b3a7210b48914814cc196e3
knumber02 <k.number.02@gmail.com>
2025-04-25 22:46:35 +0900

    add: IAMロール追加

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,24 @@ provider "aws" {
 }
 data "aws_caller_identity" "current" {}
 
-
+# Lambdaのデプロイパッケージを保存するS3バケット
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = var.app_name
+}
+
+# Lambda関数用のIAMロール
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.app_name}-lambda-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
