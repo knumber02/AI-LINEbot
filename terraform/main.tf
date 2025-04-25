7bacc6a2786e7f99e17f6dd8a937daff089ff9b1
knumber02 <k.number.02@gmail.com>
2025-04-26 08:10:06 +0900

    add: API Gateway作成

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,3 +41,37 @@ resource "aws_lambda_function" "api_lambda" {
     }
   }
 }
+
+# API Gateway
+resource "aws_apigatewayv2_api" "lambda_api" {
+  name          = "${var.app_name}-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "lambda_stage" {
+  api_id      = aws_apigatewayv2_api.lambda_api.id
+  name        = var.environment
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id             = aws_apigatewayv2_api.lambda_api.id
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+  integration_uri    = aws_lambda_function.api_lambda.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "lambda_route" {
+  api_id    = aws_apigatewayv2_api.lambda_api.id
+  route_key = "ANY /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+# Lambda関数にAPI Gatewayからの実行許可を付与
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*/*"
+} 
