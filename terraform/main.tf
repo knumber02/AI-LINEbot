fc27cd83bd4dcbe55ee5b0757ac4bcc85b252cb3
knumber02 <k.number.02@gmail.com>
2025-04-26 07:55:24 +0900

    add: lambda作成

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,3 +24,20 @@ resource "aws_iam_role" "lambda_role" {
     ]
   })
 }
+
+# Lambda関数
+resource "aws_lambda_function" "api_lambda" {
+  function_name = var.app_name
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "demo_app.lambda_handler.handler"
+  runtime       = "python3.9"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.bucket
+  s3_key    = "lambda_package.zip"
+
+  environment {
+    variables = {
+      STAGE = var.environment
+    }
+  }
+}
