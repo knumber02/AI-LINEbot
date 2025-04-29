bdf9c6ace73c13433b71f0505c95693cfd956ff4
knumber02 <k.number.02@gmail.com>
2025-04-29 14:06:47 +0900

    add: インターネットゲートウェイ&ルートテーブル作成

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,6 +38,11 @@ resource "aws_lambda_function" "api_lambda" {
   environment {
     variables = {
       STAGE = var.environment
+      DB_HOST = var.db_host
+      DB_PORT = var.db_port
+      DB_USER = var.db_username
+      DB_PASSWORD = var.db_password
+      DB_NAME = var.db_name
     }
   }
 }
@@ -109,6 +114,30 @@ resource "aws_vpc" "main" {
   }
 }
 
+# インターネットゲートウェイ
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.app_name}-igw"
+  }
+}
+
+# ルートテーブル
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.app_name}-public-rt"
+  }
+}
+
+
 # サブネット
 # Subnet1（AZ1）
 resource "aws_subnet" "db_public1" {
@@ -132,6 +161,16 @@ resource "aws_subnet" "db_public2" {
   tags = {
     Name = "${var.app_name}-db-public-subnet-2"
   }
+}
+
+# ルートテーブルとサブネットの関連付け
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.db_public1.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.db_public2.id
+  route_table_id = aws_route_table.public.id
 }
 
 # RDSのサブネットグループ
